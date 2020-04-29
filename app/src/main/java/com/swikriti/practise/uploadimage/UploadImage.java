e88bd7f3c0f4c392f34d1fb7bfcbbf2a72555a81
Swikriti <swikriti808@gmail.com>
2020-04-29 18:43:59 +0545

    third commit

--- app/src/main/java/com/swikriti/practise/uploadimage/UploadImage.java
+++ app/src/main/java/com/swikriti/practise/uploadimage/UploadImage.java
@@ -1,8 +1,10 @@
 package com.swikriti.practise.uploadimage;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,12 +12,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -29,14 +34,19 @@ public class UploadImage extends AppCompatActivity implements View.OnClickListen
     private Button buttonUpload;
     private ImageView imageView;
     private EditText editText;
+    private TextView test, test1;
     Uri selectedImage;
+    private static final int STORAGE_PERMISSION_CODE = 123;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestStoragePermission();
         buttonChoose=(Button) findViewById(R.id.buttonChoose);
         buttonUpload=(Button)findViewById(R.id.buttonUpload);
         imageView=(ImageView) findViewById(R.id.imageView);
         editText=(EditText)findViewById(R.id.editTextName);
+        test=(TextView) findViewById(R.id.test);
+        test1=(TextView) findViewById(R.id.test1);
         buttonChoose.setOnClickListener(this);
         buttonUpload.setOnClickListener(this);
 
@@ -47,8 +57,8 @@ public class UploadImage extends AppCompatActivity implements View.OnClickListen
             selectImage(UploadImage.this);
         }
         if (v == buttonUpload) {
-            //uploadImage();
-            Toast.makeText(this, "to-be-handeled", Toast.LENGTH_LONG).show();
+            uploadImage();
+
         }
 
 
@@ -77,6 +87,13 @@ public class UploadImage extends AppCompatActivity implements View.OnClickListen
         builder.show();
 
     }
+    public void uploadImage(){
+        final String name = "hello";
+        //getting the actual path of the image editText.getText().toString().trim();
+        final String path = getPath(selectedImage);
+        test.setText("hi");
+       //test1.setText(path);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -94,7 +111,7 @@ public class UploadImage extends AppCompatActivity implements View.OnClickListen
                         selectedImage = data.getData();
                         String[] filePathColumn = {MediaStore.Images.Media.DATA};
                         if (selectedImage != null) {
-                            Bitmap bitmap = null;
+                            Bitmap bitmap;
                             try {
                                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
                                 imageView.setImageBitmap(bitmap);
@@ -119,5 +136,38 @@ public class UploadImage extends AppCompatActivity implements View.OnClickListen
             }
 
         }
+    }
+    //method to get the file path from uri
+    public String getPath(Uri uri) {
+
+        Cursor cursor = getContentResolver().query(uri,null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+    //ask permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+//            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            Toast.makeText(this, "You won't be able to use this app without granting permission", Toast.LENGTH_LONG).show();
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+       // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, STORAGE_PERMISSION_CODE);
     }
 }
